e2056766cd35054a0dbeca2783e095480bb8da5e
Denis Tokarev <den4@me.com>
2018-03-10 12:38:06 +0300

    calculator ready

---
+++ js/main.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+
+    //variables
+    var modelSpecs,
+        modelPrice,
+        modelSpecsHolder,
+        modelPriceHolder,
+        modelPriceUSDHolder;
+
+    modelSpecsHolder = $('#modelSpecs');
+    modelPriceHolder = $('#modelPrice');
+    modelPriceUSDHolder = $('#modelPriceUSD');
+    modelPrice = 0;
+    modelSpecs = '';
+
+    //price calculator
+    function calculatePrice(){
+        var modelPriceEngine = $('input[name=engine]:checked', '#autoForm').val();
+        var modelPriceTransmission = $('input[name=transmission]:checked', '#autoForm').val();
+        var modelPricePackage = $('input[name=package]:checked', '#autoForm').val();
+        
+        modelPriceEngine = parseInt(modelPriceEngine);
+        modelPriceTransmission = parseInt(modelPriceTransmission);
+        modelPricePackage = parseInt(modelPricePackage);
+
+        modelPrice = modelPriceEngine + modelPriceTransmission + modelPricePackage;
+
+        modelPriceHolder.text(addSpace(modelPrice) + ' рублей');
+    };
+
+
+    //display all specs in modelSpecsHolder
+    function compileSpecs(){
+        modelSpecs = $('input[name=engine]:checked + label', '#autoForm').text() + '<br/>';
+        modelSpecs = modelSpecs + $('input[name=transmission]:checked + label', '#autoForm').text() + '<br/>';
+        modelSpecs = modelSpecs + $('input[name=package]:checked + label', '#autoForm').text() + '.';
+        modelSpecsHolder.html(modelSpecs);
+    };
+
+
+
+    //run on input selection change
+    $('#autoForm input').on('change', function(){
+        calculatePrice();
+        compileSpecs();
+        calculateUSD();
+    });
+
+
+    //run on document ready
+    calculatePrice();
+    compileSpecs();
+
+
+    //color selection
+    $('#colorsSelector .colorItem').on('click', function() {
+        var imagePath = $(this).attr('data-img-path');
+        $('#imgHolder img').attr('src', imagePath);
+    });
+
+    //adding spaces to modelPriceHolder
+    function addSpace(nStr) {
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ' ' + '$2');
+        }
+        return x1 + x2;
+    };
+
+    //RubToUsd Converter
+    var currencyUrl = 'https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+"USDRUB,EURRUB"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+    var rurUsdRate = 0;
+
+    $.ajax({
+        url: currencyUrl,
+        cache: false,
+        success: function(html){
+            /*rurUsdRate = html.query.results.rate[0].Rate;*/
+            rurUsdRate = 53; /* hardcoded until currencyURL fix*/
+            calculateUSD();
+        }
+    });
+    
+    function calculateUSD(){
+        var modelPriceUSD = modelPrice / rurUsdRate;
+        modelPriceUSDHolder.text('$ ' + addSpace( modelPriceUSD.toFixed(0) ) );
+    }
+
+});
